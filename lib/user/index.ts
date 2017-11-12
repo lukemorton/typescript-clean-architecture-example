d280207889aa9f3d492e9b726dfcb0071c1232e7
Luke Morton <lukemorton.dev@gmail.com>
2017-11-12 10:01:25 +0000

    Use real HTTP gateway

--- lib/user/index.ts
+++ lib/user/index.ts
@@ -2,7 +2,7 @@ declare function require(string): any;
 
 const partial = require('lodash.partial')
 
-import * as FakeUserGateway from './FakeUserGateway'
+import * as HTTPUserGateway from './HTTPUserGateway'
 
 import _LoadUserProfile from './LoadUserProfile'
-export const LoadUserProfile = partial(_LoadUserProfile, FakeUserGateway)
+export const LoadUserProfile = partial(_LoadUserProfile, HTTPUserGateway)
