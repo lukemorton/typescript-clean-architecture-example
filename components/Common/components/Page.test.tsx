6099a35c175bb384d026ef62d7da96901de9ddc3
Luke Morton <luke@Lukes-MacBook-Pro.local>
2017-11-26 11:53:32 +0000

    Fix test for Page

--- components/Common/components/Page.test.tsx
+++ components/Common/components/Page.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import Page from './Page'
 
 describe('<Page />', () => {
   it('should use children', () => {
-    const page = shallow(<Page title='title'>Cool</Page>)
+    const page = mount(<Page title='title'>Cool</Page>)
     expect(page).toIncludeText('Cool')
   })
 })
